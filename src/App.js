cbd5d6119e2e2a0aa4176e5a9c34f31bed3ce18a
Irina <kisilirina@ya.ru>
2021-06-09 01:32:59 +0300

    create app, get 20 results

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import CardsList from './components/CardsList/CardsList';
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+   fetch('https://randomuser.me/api/?results=20')
+   .then(result => result.json())
+   .then(data => console.log(data));
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CardsList />
   );
 }
 
